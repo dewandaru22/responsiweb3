13cb09a16054dfc9b322ba27233ab2ba248f9e3a
Riacup <supriyati98@mail.ugm.ac.id>
2019-05-23 09:52:34 +0700

    Updates

---
+++ precache-manifest.e40751da1477d05fc8062dc32fefabb5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00336fed7acb601970c0ed607e5a7432",
+    "url": "/responsiweb3/index.html"
+  },
+  {
+    "revision": "8ab0dc7973feca82f6f2",
+    "url": "/responsiweb3/static/css/main.ba49ec77.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/responsiweb3/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "8ab0dc7973feca82f6f2",
+    "url": "/responsiweb3/static/js/main.4efd4fff.chunk.js"
+  },
+  {
+    "revision": "b3cf34c6c7e486bae153",
+    "url": "/responsiweb3/static/js/runtime~main.cf154cc8.js"
+  }
+]);
